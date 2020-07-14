f64c12472a09baddd651fddfc0afd2fd0e7c6030
Renan Pereira <renanop@poli.ufrj.br>
2020-07-14 20:26:34 -0300

    Project initialized with the message Hello World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
